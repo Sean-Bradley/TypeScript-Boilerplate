bfa718b993d8116ce05d4752bc62834fcfe5aa2d
Sean Bradley <seanwasere@gmail.com>
2020-08-19 15:52:14 +0100

    updates

--- src/server/server.ts
+++ src/server/server.ts
@@ -12,6 +12,8 @@ class App {
         this.port = port
         const app = express()
         app.use(express.static(path.join(__dirname, '../client')))
+        app.use('/jquery', express.static(path.join(__dirname, '../../node_modules/jquery/dist')))
+        app.use('/bootstrap', express.static(path.join(__dirname, '../../node_modules/bootstrap/dist')))
         
         this.server = new http.Server(app);
     }
